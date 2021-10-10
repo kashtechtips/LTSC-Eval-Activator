377ea8a2d672a54a2039ded9caac4122719a4a3e
Oliver's Tech Corner <89639839+oliverstech@users.noreply.github.com>
2021-10-10 15:32:20 +0100

    Upload new ActivateScript

---
+++ data/ActivateScript.bat
@@ -0,0 +1,49 @@
+@echo off
+
+ call :isAdmin
+
+ if %errorlevel% == 0 (
+    goto :run
+ ) else (
+    echo Requesting administrative privileges...
+    goto :UACPrompt
+ )
+
+ exit /b
+
+ :isAdmin
+    fsutil dirty query %systemdrive% >nul
+ exit /b
+
+ :run
+  <YOUR BATCH SCRIPT HERE>
+ exit /b
+
+ :UACPrompt
+   echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+   echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+   "%temp%\getadmin.vbs"
+   del "%temp%\getadmin.vbs"
+  exit /B`
+
+rem -------------------
+
+cscript.exe %windir%\system32\slmgr.vbs /rilc
+cscript.exe %windir%\system32\slmgr.vbs /upk >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ckms >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /cpky >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+sc config LicenseManager start= auto & net start LicenseManager
+sc config wuauserv start= auto & net start wuauserv
+clipup -v -o -altto c:\
+
+rem -------------------
+echo Setting KMS Server...
+slmgr /skms kms.digiboy.ir
+echo Finilazing Activation...
+taskkill /f /im explorer.exe
+start explorer.exe
+slmgr /ato
+start ms-settings:activation
+exit
