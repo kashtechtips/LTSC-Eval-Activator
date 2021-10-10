f15f7da2b35d81c3c8d82c09f56f2b9826a71518
Oliver's Tech Corner <89639839+oliverstech@users.noreply.github.com>
2021-10-10 15:26:59 +0100

    Add files via upload

---
+++ data/PreActivateScript.bat
@@ -0,0 +1,9 @@
+cscript.exe %windir%\system32\slmgr.vbs /rilc
+cscript.exe %windir%\system32\slmgr.vbs /upk >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ckms >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /cpky >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+sc config LicenseManager start= auto & net start LicenseManager
+sc config wuauserv start= auto & net start wuauserv
+clipup -v -o -altto c:\
+echo
